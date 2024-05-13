b9ea06490bc577057d08f6074c4978246efa282a
Caio Fabricius <fabriciuscaio@gmail.com>
2024-05-13 15:54:49 -0300

    2024-05-13

---
+++ aula8/index.js
@@ -0,0 +1,23 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC é de 25.925925925925924
+Luiz Otávio nasceu em 1980
+*/
+
+const nome = 'Caio';
+const sobrenome = 'Fabricius';
+const idade = 32;
+const peso = 75;
+const alturaEmM = 1.75;
+let indiceMassaCorporal;
+let anoNascimento;
+
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2024 - idade;
+
+// template strings
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`)
+console.log(`${nome} ${sobrenome} nasceu em ${anoNascimento}.`)
+
