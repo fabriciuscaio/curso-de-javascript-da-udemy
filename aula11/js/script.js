e8a66ce26b25e3949025ca6d916cb563aef55e13
Caio Fabricius <fabriciuscaio@gmail.com>
2024-06-05 12:33:13 -0300

    2024-06-05_2

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,1 +1,18 @@
-alert('Com a nossa mensagem.');
+/**
+ * alert('Com a nossa mensagem.');
+ * mostra um alerta
+ * confirm('Deseja mesmo apagar?');
+ * opção Ok ou Cancelar e retorna boolean true ou false
+ * prompt('Escreva seu nome');
+ * abre caixa pra escrever e retorna o que foi escrito
+**/
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite um número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado foi ${num1 + num2}`);
+
+/* numa template string ${} você pode colocar a conta pra fazer ao invés de colocar a conta numa const ou let, igual fiz aqui */
