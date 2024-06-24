be776eeb3a67d9ab006902436b6806531566dc51
Caio Fabricius <fabriciuscaio@gmail.com>
2024-06-24 15:17:03 -0300

    2024-06-24

--- aula14/index.js
+++ aula14/index.js
@@ -29,3 +29,44 @@ let num6 = 3.3;
 console.log(Number.isInteger(num5));
 console.log(Number.isInteger(num6));
 
+// para saber se o resultado é um NaN (not a number), usa-se o Number.isNaN()
+let temp = num1 * 'Ola';
+console.log(temp);
+console.log(Number.isNaN(temp));
+
+let yupi = num1 * 5;
+console.log(Number.isNaN(yupi));
+
+let lalala = '5';
+console.log(typeof(lalala));
+lalala = Number('5');
+console.log(typeof(lalala));
+
+// O padrão de contas do Javascript é no padrão IEEE 754-2008, que possui imprecisões
+
+let numA = 0.7;
+let numB = 0.1;
+
+console.log(numA + numB);
+
+numA += numB; // ou seja, numA = numA + numB
+
+console.log(numA);
+
+numA += numB;
+numA += numB;
+
+console.log(numA);
+
+// quando usa o toFixed, que seria pra mostrar um número limitados de caracteres depois do ".", ele já fica em 1.0
+
+console.log(numA.toFixed(2));
+
+// apesar de ter casas decimais (zero) depois do ponto, um 1.00 seria considerado um número inteiro, mas se usamos o número acima, que virou 1.00 depois do toFixed, ele mostra como não sendo inteiro
+
+let um = 1.00
+
+console.log(Number.isInteger(um));
+console.log(Number.isInteger(numA));
+
+num1 = parseFloat(num1);
