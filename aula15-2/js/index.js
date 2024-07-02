eb007963dd8ffdbd15bf05b02f61b7e51a4294ed
Caio Fabricius <fabriciuscaio@gmail.com>
2024-07-01 21:30:58 -0300

    2024-07-01

--- aula15-2/js/index.js
+++ aula15-2/js/index.js
@@ -1,2 +1,4 @@
-window.prompt('Digite um número')
+const numero = prompt('Digite um número:');
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
 
